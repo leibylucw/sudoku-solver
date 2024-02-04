d15bd634467506278f9ca35904ba5ada2c716e75
Luke Leiby <lucaswleiby@gmail.com>
2024-02-04 16:22:21 -0500

    Add basic Sudoku board validation check

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -3,3 +3,10 @@ def createSudokuBoard(filePath):
 		sudokuBoard = [line.strip() for line in sudokuFile.readlines()]
 
 	return sudokuBoard
+
+def validateSudokuBoard(sudokuBoard):
+	for row in sudokuBoard:
+		if not all(character.isdigit() or character == '_' for character in row):
+			return False
+
+	return True
